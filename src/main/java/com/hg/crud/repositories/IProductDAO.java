3926468c200e90f6d2c33418a023339b37de6b18
Jose luis <109111140+Lokyre@users.noreply.github.com>
2024-01-24 21:55:43 -0500

    Delete src directory

--- src/main/java/com/hg/crud/repositories/IProductDAO.java
+++
@@ -1,9 +0,0 @@
-package com.hg.crud.repositories;
-
-import com.hg.crud.models.ProductDTO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IProductDAO extends MongoRepository<ProductDTO, String> {
-}
